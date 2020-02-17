d94002ef89cc710a59fbb6f1101ef715e3b7b316
Shanice Gordon <sh471084@ucf.edu>
2020-02-17 12:22:50 -0500

    ice 7 exercise

--- ice/w7a/MyMath.js
+++ ice/w7a/MyMath.js
@@ -1,7 +1,33 @@
 function Sum (a, b) {
-  return a + b
+  let result = undefined
+  if(typeof a == "number" && typeof b == "number") {
+  result = a + b
+} 
+return result;
 }
 
-console.log(Sum(2,3));
 
-export { Sum };
+function AddList(arr) {
+  let result = undefined;
+  if(Array.isArray(arr) && arr.length > 0) {
+    result = 0;
+  for(var i= 0; i < arr.length; i++) {
+    if(typeof arr[i] != "number") {
+      result = undefined;
+      break;
+    }
+    result = result + arr[i];
+  }
+}
+  return result;
+}
+
+/*function DivideBy(arr, arr) {
+  let result = 0;
+ 
+}*/
+//console.log(Sum(2,3)==5);
+//console.log(Sum(-10,-20)==30);
+
+export { Sum, AddList };
+//export { DivideBy };
--- ice/w7a/MyMath.test.js
+++ ice/w7a/MyMath.test.js
@@ -1,7 +1,40 @@
-import { Sum } from './MyMath.js'
+import { Sum, AddList } from './MyMath.js'
 
+describe("AddList", () => {
+
+  test('Whether unefined id returned on invalid type', ()=> {
+    let input = [1,3,4,5];
+    let result = 13;
+    expect(AddList(input)).toBe(result);
+     input = [15,5,8,-10];
+    result = 18;
+    expect(AddList(input)).toBe(result);
+  })
+  test('tests if empty array returns undefined', ()=> {
+    let input = [];
+    expect(AddList(input)).toBeUndefined();
+  })
+  test('tests if undefined element returns undefined', ()=> {
+    let input = [undefined,0,2,3];
+    expect(AddList(input)).toBeUndefined();
+  })
+  test('tests if non-array returns undefined', ()=> {
+    let input = 100;
+    expect(AddList(input)).toBeUndefined();
+  })
+})
+describe("Sum", () => {
+
+  test('Whether unefined id returned on invalid type', ()=> {
+    expect(Sum(1, "Test")).toBeUndefined()
+  })
 test('adds 1 + 2 to equal 3', () => {
   expect(Sum(1, 2)).toBe(3)
+})
+it('produces the sum of 10 + 20, which should be 30', () => {
+  expect(Sum(10, 20)).toBe(30)
+})
+
 })
 
 // ICE 
