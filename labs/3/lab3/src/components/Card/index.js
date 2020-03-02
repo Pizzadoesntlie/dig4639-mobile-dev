35c6703a5b807ba8a91574e906c71517acf59a01
Shanice Gordon <sh471084@ucf.edu>
2020-03-02 12:35:49 -0500

    lab 3 fixed

--- labs/3/lab3/src/components/Card/index.js
+++ labs/3/lab3/src/components/Card/index.js
@@ -2,19 +2,10 @@ import React from 'react';
 import "./index.css";
 
 class Card extends React.Component {
-    constructor(props) {
-        super(props)
-        this.props = props;
-        this.element = document.createElement("div");
-        let span = document.createElement("span");
-        this.element.appendChild(span);
-        span.innerHTML = this.props.content;
-        this.element.className = "card";
-    }
     render() {
 
         return (
-            <div>
+            <div className = "card">
            {this.props.content}
            </div>
         )
